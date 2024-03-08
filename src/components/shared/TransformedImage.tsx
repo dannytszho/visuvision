0dc0a4505e236fcbb89a02c278387d480c211418
dannytszho <dannytszho@gmail.com>
2024-03-08 01:09:00 -0600

    add Media Uploader

---
+++ src/components/shared/TransformedImage.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Image from "next/image";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+import { CldImage } from "next-cloudinary";
+
+const TransformedImage = ({
+  image,
+  type,
+  title,
+  isTransforming,
+  setIsTransforming,
+  transformationConfig,
+  hasDownload = false,
+}: TransformedImageProps) => {
+  const downloadHandler = () => {};
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex-between">
+        <h3 className="h3-bold text-dark-600">TransformedImage</h3>
+        {hasDownload && (
+          <button className="download-btn" onClick={downloadHandler}>
+            <Image
+              src="assets/icons/download.svg"
+              alt="Download"
+              width={24}
+              height={24}
+              className="pb-[6px]"
+            />
+          </button>
+        )}
+      </div>
+      {image?.publicId && transformationConfig ? (
+        <div className="relative">
+          <CldImage
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt={image.title}
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="transformed-image"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false);
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false);
+              }, 5000);
+            }}
+          />
+        </div>
+      ) : (
+        <div className="transformed-placeholder">Transformed image</div>
+      )}
+    </div>
+  );
+};
+
+export default TransformedImage;
